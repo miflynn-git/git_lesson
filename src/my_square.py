5dabe1fbe9b685951eec9406c5d9753beaa233a9
Mikhaela Flynn <miflynn-af@umass.edu>
2019-10-01 10:15:21 -0400

    updated my_square.py to match the lesson

--- src/my_square.py
+++ src/my_square.py
@@ -1,6 +1,7 @@
-def my_square(x):
+def my_square(y):
 	"""takes a value and returns the squared value
+	uses the * operator
 	"""
-	return(x**2)
+	return(y**2)
 
-print(my_square(3))
+print(my_square(42))
